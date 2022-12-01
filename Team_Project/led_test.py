e4709c63d0b9589e8cc5b1a33b088e9553fc1093
min02choi@sookmyung.ac.kr <min02choi@sookmyung.ac.kr>
2022-12-01 10:05:30 +0900

    [PC:2022-11-30] [PJ]Led Test(opX)

--- Team_Project/led_test.py
+++ Team_Project/led_test.py
@@ -13,27 +13,6 @@ def signal_handler(signal, frame):
 signal.signal(signal.SIGINT, signal_handler)
 
 
-#### MQ7 ####
-# 일산화탄소 수치 감지
-def MQ7_detect():
-    global CO_STATE
-    global LEVEL
-
-    # CO_STATE 값 측정
-
-    # CO 수치에 따른 LEVEL 설정
-    while True:
-        if (20 > CO_STATE):
-            LEVEL = 0
-        elif (40 > CO_STATE >= 20):
-            LEVEL = 1
-        elif (60 > CO_STATE >= 40):
-            LEVEL = 2
-        elif (CO_STATE >= 60):
-            LEVEL = 3
-        time.sleep(1)
-
-
 #### LED ####
 # LED 색 RGB 설정
 def setRGB(r, g, b):
@@ -52,14 +31,6 @@ def led_action():
     # 1단계, 2단계, 3단계 LED가 켜지는 시간을 조절할 것 (led_speed)
     # time.sleep(led_speed), time.sleep(1 - led_speed) 이런 느낌으로
     while True:
-        # 여기를 if문 안으로 넣을 것
-        # setRGB(rgb_color[0], rgb_color[1], rgb_color[2])
-        # time.sleep(led_speed)
-        # setRGB(rgb_color[0], rgb_color[1], rgb_color[2])         # LED 꺼짐
-        # time.sleep(led_speed)   # 계산 해서 1 - led_speed
-
-        # 근데 변수를 다 global로 빼면 if문으로 안나눠도 되긴하는데...
-        # 코드의 효율성 측면은 나중에 고려. 일단은 동작여부 우선
         if (LEVEL == 0):
             setRGB(0, 0, 0)
         if (LEVEL == 1):
@@ -77,44 +48,6 @@ def led_action():
             time.sleep(led_speed)
             setRGB(0, 0, 0)
             time.sleep(0.2)
-
-
-#### Piezo ####
-# 피에조부저
-def piezo_action():
-    global LEVEL
-
-    while True:
-        if (LEVEL == 0):
-            PWM_Piezo.changeDutyCycle(20)
-            time.sleep(1)
-            PWM_Piezo.changeDutyCycle(0)
-            time.sleep(1)
-        if (LEVEL == 1):
-            PWM_Piezo.changeDutyCycle(20)
-            time.sleep(1)
-            PWM_Piezo.changeDutyCycle(0)
-            time.sleep(1)
-        if (LEVEL == 2):
-            PWM_Piezo.changeDutyCycle(20)
-            time.sleep(1)
-            PWM_Piezo.changeDutyCycle(0)
-            time.sleep(1)
-        if (LEVEL == 2):
-            PWM_Piezo.changeDutyCycle(20)
-            time.sleep(1)
-            PWM_Piezo.changeDutyCycle(0)
-            time.sleep(1)
-
-
-
-
-
-#### Vibration ####
-# 진동모터
-def vibration_action(speed):
-    global LEVEL
-
 
 
 ################### 센서&변수 세팅 ################################
@@ -160,28 +93,14 @@ GPIO.setup(piezo, GPIO.OUT)
 PWM_Piezo = GPIO.PWM(piezo, 100)
 PWM_Piezo.start(0)
 
-
-#### Vibration ####
-# 이후 세팅
-
-
-#### Variable ####
-# 함수 안에서 global 설정
-# 활성화 시킬것
 LEVEL = 0
-
 
 ##################### 코드 시작 ##############################
 
 try:
     # 쓰레드 실행
-    thread0_MQ7 = threading.Thread(target=MQ7_detect, daemon=True)
-    thread0_MQ7.start()
-
     thread1_LED = threading.Thread(target=led_action, daemon=True)
     thread1_LED.start()
-    thread2_PIEZO = threading.Thread(target=piezo_action, daemon=True)
-    thread2_PIEZO.start()
 
     # state 설정
     #
@@ -192,35 +111,25 @@ try:
 
         # LEVEL 값에 따른 작동
         if (LEVEL == 0):
-            temp = 0
+            rgb_color = [0, 0, 0]
             # 택트 버튼 누를 경우 LEVEL 값을 0으로 설정
 
         #### 1단계 ####
         if (LEVEL == 1):
             ## LED 설정 ##
             rgb_color = [255, 255, 0]   # 색 수치만 넣으면 됨
-            led_speed = 1           # LED 깜빡이는 속도
-
-            ## Piezo 설정 ##
-
-            ## Vibration 설정 ##
-
+            led_speed = 1               # LED 깜빡이는 속도
 
         #### 2단계 ####
         if (LEVEL == 2):
-            rgb_color = [255, 130, 0]  # 색 수치만 넣으면 됨
-            led_speed = 1  # LED 깜빡이는 속도
+            rgb_color = [255, 130, 0]   # 색 수치만 넣으면 됨
+            led_speed = 0.6             # LED 깜빡이는 속도
 
         #### 3단계 ####
         if (LEVEL == 3):
-            rgb_color = [255, 0, 0]  # 색 수치만 넣으면 됨
-            led_speed = 1  # LED 깜빡이는 속도
+            rgb_color = [255, 0, 0]     # 색 수치만 넣으면 됨
+            led_speed = 0.3             # LED 깜빡이는 속도
 
 
 except KeyboardInterrupt:
     print("Program End")
-
-# 코드가 너무 더러워지나
-
-# 매개변수로 전달? 아니면 global 변수로
-# 소스파일 분리(led, piezo, ...)
